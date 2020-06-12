7bbee3034471d3590842fcd9a53cbd648df508e5
Marc <mdreem@fastmail.fm>
2020-06-12 17:14:08 +0200

    add basic webserver with response

---
+++ go_weather/endpoints/weather.go
@@ -0,0 +1,26 @@
+package endpoints
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func Initialize() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+
+	http.Handle("/", r)
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		fmt.Println("Error occurred while starting http server:", err)
+	}
+}
+
+func HomeHandler(writer http.ResponseWriter, _ *http.Request) {
+	writer.WriteHeader(200)
+	_, err := fmt.Fprintf(writer, "Home\n")
+	if err != nil {
+		fmt.Println("Error occurred:", err)
+	}
+}
