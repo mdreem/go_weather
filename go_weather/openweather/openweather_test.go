fbafae15c3b9ae9f2d435094a05a8a51f2315285
Marc <mdreem@fastmail.fm>
2020-06-14 19:34:32 +0200

    testcase for creating openweatherapi client

--- go_weather/openweather/openweather_test.go
+++ go_weather/openweather/openweather_test.go
@@ -8,7 +8,7 @@ import (
 
 func TestFetchWeatherForCity(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.Write([]byte("{\"id\": 12345, \"name\": \"Twin Peaks\"}"))
+		_, _ = rw.Write([]byte("{\"id\": 12345, \"name\": \"Twin Peaks\"}"))
 	}))
 	defer server.Close()
 	client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
@@ -17,5 +17,17 @@ func TestFetchWeatherForCity(t *testing.T) {
 
 	if weatherForCity.CityName != "Twin Peaks" {
 		t.Errorf("expected city to be 'Twin Peaks', but it was '%s'", weatherForCity.CityName)
+	}
+}
+
+func TestOpenWeatherFactoryMethod(t *testing.T) {
+	client := CreateClient("someApiKey")
+
+	if client.baseUrl != "https://api.openweathermap.org" {
+		t.Errorf("baseUrl is '%s'", client.baseUrl)
+	}
+
+	if client.apiKey != "someApiKey" {
+		t.Errorf("expected apiKey to be 'someApiKey' but is was '%s'", client.apiKey)
 	}
 }
