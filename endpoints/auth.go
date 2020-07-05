e985903614f14fc0d1c9b63721141ad45ccb698a
Marc <mdreem@fastmail.fm>
2020-07-05 20:54:02 +0200

    refactoring

--- endpoints/auth.go
+++ endpoints/auth.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/oauth2"
 	"log"
 	"net/http"
+	"strings"
 )
 
 type auth struct {
@@ -20,6 +21,76 @@ type authConfig interface {
 	GetClientID() string
 }
 
+type OAuth2Config struct {
+	config *oauth2.Config
+}
+
+func initializeOpenIdConnect(clientSecret string) *auth {
+	clientID := "weather"
+	redirectUrl := "http://localhost:8000/oidc/callback"
+
+	ctx := context.Background()
+
+	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/Weather")
+	if err != nil {
+		panic(err)
+	}
+
+	oauth2Config := oauth2.Config{
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		RedirectURL:  redirectUrl,
+
+		Endpoint: provider.Endpoint(),
+		Scopes:   []string{oidc.ScopeOpenID, "profile", "email"},
+	}
+
+	oidcConfig := &oidc.Config{
+		ClientID: clientID,
+	}
+
+	var verifier = provider.Verifier(oidcConfig)
+
+	auth := auth{
+		oauth2Config: OAuth2Config{config: &oauth2Config},
+		verifier:     verifier,
+		ctx:          &ctx,
+	}
+	return &auth
+}
+
+func (auth auth) authenticationHandlerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		auth.authenticationHandler(next, w, r)
+	})
+}
+
+func (auth auth) authenticationHandler(next http.Handler, writer http.ResponseWriter, request *http.Request) {
+	rawAccessToken := request.Header.Get("Authorization")
+
+	if rawAccessToken == "" {
+		log.Printf("Token missing.")
+		writer.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	parts := strings.Split(rawAccessToken, " ")
+	if len(parts) != 2 {
+		log.Printf("Token malformed.")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	_, err := auth.verifier.Verify(*auth.ctx, parts[1])
+	if err != nil {
+		log.Printf("Token could not be verified. %v", err)
+		writer.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	next.ServeHTTP(writer, request)
+}
+
 func (config OAuth2Config) Exchange(ctx context.Context, code string, opts ...oauth2.AuthCodeOption) (*oauth2.Token, error) {
 	return config.config.Exchange(ctx, code, opts...)
 }
@@ -32,24 +103,24 @@ func (config OAuth2Config) GetClientID() string {
 	return config.config.ClientID
 }
 
-func (auth auth) handleOAuth2Callback(w http.ResponseWriter, r *http.Request) {
-	oauth2Token, err := auth.oauth2Config.Exchange(*auth.ctx, r.URL.Query().Get("code"))
+func (auth auth) handleOAuth2Callback(writer http.ResponseWriter, request *http.Request) {
+	oauth2Token, err := auth.oauth2Config.Exchange(*auth.ctx, request.URL.Query().Get("code"))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		writer.WriteHeader(http.StatusInternalServerError)
 		log.Printf("could not exchange code for token: %v", err)
 		return
 	}
 
 	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
 	if !ok {
-		w.WriteHeader(http.StatusInternalServerError)
+		writer.WriteHeader(http.StatusInternalServerError)
 		log.Printf("missing token")
 		return
 	}
 
-	_, err = w.Write([]byte(rawIDToken))
+	_, err = writer.Write([]byte(rawIDToken))
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		writer.WriteHeader(http.StatusInternalServerError)
 		log.Printf("not able to write token")
 		return
 	}
--- endpoints/routes.go
+++ endpoints/routes.go
@@ -1,13 +1,7 @@
 package endpoints
 
 import (
-	"context"
-	"github.com/coreos/go-oidc"
 	"github.com/gorilla/mux"
-	"golang.org/x/oauth2"
-	"log"
-	"net/http"
-	"strings"
 )
 
 func (c WeatherDataController) SetupRoutes() *mux.Router {
@@ -26,74 +20,4 @@ func (c WeatherDataController) SetupRoutes() *mux.Router {
 	oidcHandler.HandleFunc("/login", auth.handleLogin).Methods("GET")
 
 	return r
-}
-
-type OAuth2Config struct {
-	config *oauth2.Config
-}
-
-func initializeOpenIdConnect(clientSecret string) *auth {
-	clientID := "weather"
-	redirectUrl := "http://localhost:8000/oidc/callback"
-
-	ctx := context.Background()
-
-	provider, err := oidc.NewProvider(ctx, "http://localhost:8080/auth/realms/Weather")
-	if err != nil {
-		panic(err)
-	}
-
-	oauth2Config := oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		RedirectURL:  redirectUrl,
-
-		Endpoint: provider.Endpoint(),
-		Scopes:   []string{oidc.ScopeOpenID, "profile", "email"},
-	}
-
-	oidcConfig := &oidc.Config{
-		ClientID: clientID,
-	}
-
-	var verifier = provider.Verifier(oidcConfig)
-
-	auth := auth{
-		oauth2Config: OAuth2Config{config: &oauth2Config},
-		verifier:     verifier,
-		ctx:          &ctx,
-	}
-	return &auth
-}
-
-func (auth auth) authenticationHandlerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		auth.authenticationHandler(next, w, r)
-	})
-}
-
-func (auth auth) authenticationHandler(next http.Handler, writer http.ResponseWriter, request *http.Request) {
-	rawAccessToken := request.Header.Get("Authorization")
-
-	if rawAccessToken == "" {
-		log.Printf("Token missing.")
-		writer.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	parts := strings.Split(rawAccessToken, " ")
-	if len(parts) != 2 {
-		log.Printf("Token malformed.")
-		writer.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	_, err := auth.verifier.Verify(*auth.ctx, parts[1])
-	if err != nil {
-		log.Printf("Token could not be verified. %v", err)
-		writer.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	next.ServeHTTP(writer, request)
 }
