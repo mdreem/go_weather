219866528b10e7846108c61fb74dcf01eb2c476f
Marc <mdreem@fastmail.fm>
2020-07-06 21:37:19 +0200

    refactor tests

--- openweather/openweather_test.go
+++ openweather/openweather_test.go
@@ -10,7 +10,7 @@ import (
 func TestFetchWeatherForCity(t *testing.T) {
 	assertion := assert.New(t)
 
-	server := createTestServer(200)
+	server := createTestServer(200, t)
 	defer server.Close()
 	client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
 
@@ -30,71 +30,82 @@ func TestOpenWeatherFactoryMethod(t *testing.T) {
 func TestFetchWeatherForCityInvalidUrl(t *testing.T) {
 	assertion := assert.New(t)
 
-	server := createTestServer(200)
+	server := createTestServer(200, t)
 	defer server.Close()
 	client := Client{baseUrl: string(0x7f), apiKey: "noKey", client: server.Client()}
 
 	_, err := client.FetchWeatherForCity("Twin Peaks")
 
 	assertion.Error(err)
+	assertion.Regexp("invalid control character in URL", err.Error())
 }
 
 func TestFetchWeatherForCityInvalidTarget(t *testing.T) {
 	assertion := assert.New(t)
 
-	server := createTestServer(200)
+	server := createTestServer(200, t)
 	defer server.Close()
 	client := Client{baseUrl: "http://nothing", apiKey: "noKey", client: server.Client()}
 
 	_, err := client.FetchWeatherForCity("Twin Peaks")
 
 	assertion.Error(err)
+	assertion.Regexp("dial tcp: lookup nothing", err.Error())
 }
 
 func TestFetchWeatherForCityBadResponseCode(t *testing.T) {
 	assertion := assert.New(t)
 
-	server := createTestServer(500)
-	defer server.Close()
-	client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
+	t.Run("Bad response code", func(t *testing.T) {
+		server := createTestServer(500, t)
+		defer server.Close()
+		client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
 
-	_, err := client.FetchWeatherForCity("Twin Peaks")
+		_, err := client.FetchWeatherForCity("Twin Peaks")
 
-	assertion.EqualError(err, "wrong response code received")
+		assertion.Error(err)
+		assertion.Regexp("wrong response code received", err.Error())
+	})
 }
 
-func TestFetchWeatherForCityReadingError(t *testing.T) {
+func TestFetchWeather(t *testing.T) {
 	assertion := assert.New(t)
 
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		rw.Header().Set("Content-Length", "1")
-	}))
-	defer server.Close()
-	client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
+	t.Run("Reading error", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			rw.Header().Set("Content-Length", "1")
+		}))
+		defer server.Close()
+		client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
 
-	_, err := client.FetchWeatherForCity("Twin Peaks")
+		_, err := client.FetchWeatherForCity("Twin Peaks")
 
-	assertion.EqualError(err, "unexpected EOF")
+		assertion.EqualError(err, "unexpected EOF")
+	})
+
+	t.Run("Unmarshalling error", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+			_, err := rw.Write([]byte("<noJson></noJson>"))
+			if err != nil {
+				t.Fatal("write failed")
+			}
+		}))
+		defer server.Close()
+		client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
+
+		_, err := client.FetchWeatherForCity("Twin Peaks")
+
+		assertion.EqualError(err, "invalid character '<' looking for beginning of value")
+	})
 }
 
-func TestFetchWeatherForCityUnmarshallingError(t *testing.T) {
-	assertion := assert.New(t)
-
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		_, _ = rw.Write([]byte("<noJson></noJson>"))
-	}))
-	defer server.Close()
-	client := Client{baseUrl: server.URL, apiKey: "noKey", client: server.Client()}
-
-	_, err := client.FetchWeatherForCity("Twin Peaks")
-
-	assertion.EqualError(err, "invalid character '<' looking for beginning of value")
-}
-
-func createTestServer(statusCode int) *httptest.Server {
+func createTestServer(statusCode int, t *testing.T) *httptest.Server {
 	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.WriteHeader(statusCode)
-		_, _ = rw.Write([]byte("{\"id\": 12345, \"name\": \"Twin Peaks\"}"))
+		_, err := rw.Write([]byte("{\"id\": 12345, \"name\": \"Twin Peaks\"}"))
+		if err != nil {
+			t.Fatal("write failed")
+		}
 	}))
 	return server
 }
